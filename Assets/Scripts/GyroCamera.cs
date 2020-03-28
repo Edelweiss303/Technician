4fdb1dd75a914b85f71544b771ce12fff8c3790c
Edelweiss <barnwal.rishi@gmail.com>
2020-03-27 21:57:17 -0400

    Added: Gyro Camera control, and touch to remove holes in shader

---
+++ Assets/Scripts/GyroCamera.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GyroCamera : MonoBehaviour
+{
+    public float horizontalRange = 33.0f;
+    public float verticalRange = 29.0f;
+
+    private Gyroscope gyro;
+    private Vector3 rotationAmount;
+    private Camera camera;
+    void Awake()
+    {
+        gyro = Input.gyro;
+        gyro.enabled = true;
+
+        camera = GetComponent<Camera>();
+    }
+
+    private void Start()
+    {
+        transform.rotation = Quaternion.identity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GyroInput();
+        TouchInput();
+    }
+
+    private void GyroInput()
+    {
+        rotationAmount = gyro.rotationRateUnbiased ;
+        rotationAmount.x *= -1;
+        rotationAmount.y *= -1;
+        
+        transform.Rotate(rotationAmount);
+    }
+
+    private void TouchInput()
+    {
+        foreach(Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                RaycastHit hit;
+                Ray ray = camera.ScreenPointToRay(touch.position);
+                if(Physics.Raycast(ray, out hit))
+                {
+                    Debug.DrawRay(ray.origin, ray.direction * 100, Color.red, 3.0f);
+
+                    if (!EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+                    {
+                        GameObject obj = hit.collider.gameObject;
+                        Material material = hit.collider.gameObject.GetComponent<Renderer>().sharedMaterial;
+                        Debug.Log(material);
+                        material.SetVector("Polygon_Tiling", Vector4.zero);
+
+                    }
+
+                }
+            }
+        }
+    }
+
+    public void OnResetOrientation()
+    {
+        transform.rotation = Quaternion.identity;
+    }
+}
---
+++ Assets/Scripts/HoleGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleGenerator : MonoBehaviour
+{
+    public static float health = 100.0f;
+    private Renderer[] armorRenderers;
+    void Start()
+    {
+        armorRenderers = GetComponentsInChildren<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
